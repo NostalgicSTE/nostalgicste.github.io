fa7f766b91440dcb513e4b41b1f351e17be0683d
Disabled Jinx <47773944+DisabledAurora@users.noreply.github.com>
2022-07-15 14:18:22 +0800

    Changes to be committed: 	new file:   .vscode/settings.json 	modified:   index.html 	modified:   root/JS/swiper.js 	modified:   root/Style/index.css 	modified:   root/Style/index.scss 	modified:   root/manifest.json

--- root/JS/swiper.js
+++ root/JS/swiper.js
@@ -1,5 +1,5 @@
 /* Creating a new instance of the Swiper class. */
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".mySwiper", {
   // Optional parameters
   // effect: "fade",
   // direction: "vertical",
@@ -8,7 +8,7 @@ const swiper = new Swiper(".swiper", {
   // loop: true,
   autoplay: {
     delay: 5000,
-    disableOnInteraction: false,
+    disableOnInteraction: true,
   },
   // If we need pagination
   pagination: {
@@ -19,5 +19,51 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  grabCursor: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  direction: "vertical",
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination2",
+    dynamicBullets: true,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  // effect: "flip",
+  // effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      origin: "left center",
+      translate: ["-5%", 0, -200],
+      rotate: [0, 90, 0],
+    },
+    next: {
+      origin: "right center",
+      translate: ["5%", 0, -200],
+      rotate: [0, -90, 0],
+    },
+  },
+
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination-3",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-3",
+    prevEl: ".swiper-button-prev-3",
   },
 });
